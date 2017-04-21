6bf63688b0148925d01dcff75f2185535d1ac245
Familia Alvarez G <Familia Alvarez G@DESKTOP-RSF4LJL>
2017-04-21 16:15:36 -0500

    23

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <h1>
-            Hola
-        </h1>
+        <?php
+        // put your code here
+        ?>
     </body>
 </html>
